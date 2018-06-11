46bcc951e699b7a82a1d3798d94f3f13ccfd098c
Russell Memory <rmemory@Russells-MacBook-Pro-2.local>
2018-06-11 14:29:12 -0600

    everything but the front end

--- server/controllers/messages_controller.js
+++ server/controllers/messages_controller.js
@@ -5,6 +5,7 @@ module.exports = {
   create: ( req, res ) => {
     const { text, time } = req.body;
     messages.push({ id, text, time });
+    req.session.messages.push({id, text, time})
     id++;
     res.status(200).send( messages );
   },
@@ -15,7 +16,7 @@ module.exports = {
 
   update: ( req, res ) => {
     const { text } = req.body;
-    const updateID = req.params.id;
+    const updateID = req.query.id;
     const messageIndex = messages.findIndex( message => message.id == updateID );
     let message = messages[ messageIndex ];
 
@@ -33,5 +34,9 @@ module.exports = {
     messageIndex = messages.findIndex( message => message.id == deleteID );
     messages.splice(messageIndex, 1);
     res.status(200).send( messages );
+  },
+
+  history: (req, res, next) => {
+    res.status(200).send(req.session.messages);
   }
 };
--- server/index.js
+++ server/index.js
@@ -2,16 +2,41 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mc = require( `./controllers/messages_controller` );
 const port = 1337;
+const session = require('express-session')
 
 const app = express();
 
 app.use( bodyParser.json() );
 app.use( express.static( `${__dirname}/../build` ) );
 
+const createInitialSession = require('./middlewares/session');
+const filter = require('./middlewares/filter');
+
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+    cookie: {maxAge: 10000}
+}))
+
+app.use(createInitialSession);
+
+app.use((req, res, next) => {
+    const {method} = req;
+
+    if(method === 'POST' || method === 'PUT'){
+        filter(req, res, next)
+    }else{
+        next();
+    }
+})
+
+
 app.post( "/api/messages", mc.create );
 app.get( "/api/messages", mc.read );
 app.put( "/api/messages", mc.update );
 app.delete( "/api/messages", mc.delete );
+app.get('/api/messages/history', mc.history)
 
 
 app.listen( port, () => { console.log(`Server listening on port ${port}.`); } );
